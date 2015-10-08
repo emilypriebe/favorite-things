8a1d119505bc575665edb665c686156c34083a8f
Emily Priebe <emilypriebe@gmail.com>
2015-10-08 12:49:14 -0700

    Correct jQuery so form prints.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-$("#blanks").submit(function(event) {
-  var foodInput = $("input#food").val();
-  var restuarantInput = $("input#restaurant").val();
-  var animalInput = $("input#animal").val();
-  var countryInput = $("input#country").val();
-  var movieInput = $("input#movie").val();
-  var tvshowInput = $("input#tvshow").val();
+  $("#blanks form").submit(function(event) {
+    var foodInput = $("input#food").val();
+    var restaurantInput = $("input#restaurant").val();
+    var animalInput= $("input#animal").val();
+    var countryInput = $("input#country").val();
+    var movieInput = $("input#movie").val();
+    var tvshowInput = $("input#tvshow").val();
 
-  $(".food").text(foodInput);
-  $(".restuarant").text(restaurantInput);
-  $(".animal").text(animalInput);
-  $(".country").text(countryInput);
-  $(".movie").text(movieInput);
-  $(".tvshow").text(tvshowInput);
+    $(".food").text(foodInput);
+    $(".restaurant").text(restaurantInput);
+    $(".animal").text(animalInput);
+    $(".country").text(countryInput);
+    $(".movie").text(movieInput);
+    $(".tvshow").text(tvshowInput);
 
-  $("#list").show();
+    $("#list").show();
 
-  event.preventDefault();
+    event.preventDefault();
   });
 });
